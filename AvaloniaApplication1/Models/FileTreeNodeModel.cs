100e844922d74f1a30027343c9ff3e0286c1f055
Denis Bubalo <bubalo2000@mail.ru>
2023-04-06 15:57:12 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaApplication1/Models/FileTreeNodeModel.cs
@@ -0,0 +1,282 @@
+﻿using Avalonia.Controls.Shapes;
+using Avalonia.Threading;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication1.Models
+{
+    public class FileTreeNodeModel : ReactiveObject
+    {
+        #region FIELDS
+        private string _path;
+        private string _name;
+        private long? _size;
+        private DateTimeOffset? _modified;
+        private FileSystemWatcher? _watcher;
+        private ObservableCollection<FileTreeNodeModel>? _children;
+        private bool _hasChildren = true;
+        private bool _isExpanded;
+        private string _version;
+        private string? _hashSum;
+        private bool _isChecked;
+        #endregion
+
+        public bool IsChecked 
+        {
+            get => _isChecked;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _isChecked, value);
+                if (HasChildren)
+                {
+                    Task.Run(() =>
+                    {
+                        foreach (var child in Children)
+                        {
+                            child.IsChecked = value;
+                        }
+                    });
+                }
+            }
+        }
+        public string? HashSum
+        {
+            get => _hashSum;
+            private set => this.RaiseAndSetIfChanged(ref _hashSum, value);
+        }
+        public string Version
+        {
+            get => _version;
+            private set => this.RaiseAndSetIfChanged(ref _version, value);
+        }
+        public string Path
+        {
+            get => _path;
+            set => this.RaiseAndSetIfChanged(ref _path, value);
+        }
+        public string Name
+        {
+            get => _name;
+            private set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+        public long? Size
+        {
+            get => _size;
+            private set => this.RaiseAndSetIfChanged(ref _size, value);
+        }
+        public DateTimeOffset? Modified
+        {
+            get => _modified;
+            private set => this.RaiseAndSetIfChanged(ref _modified, value);
+        }
+        public bool HasChildren
+        {
+            get => _hasChildren;
+            private set => this.RaiseAndSetIfChanged(ref _hasChildren, value);
+        }
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set => this.RaiseAndSetIfChanged(ref _isExpanded, value);
+        }
+
+        public bool IsDirectory { get; }
+        public FileTreeNodeModel? Parent { get; set; }
+        public ObservableCollection<FileTreeNodeModel> Children => _children ??= LoadChildren();
+        public string ModifiedToString => IsDirectory ? Directory.GetCreationTime(_path).ToString() : Modified == null ? "-" : $"{Modified.Value}";
+
+        public FileTreeNodeModel(string path, bool isDirectory, FileTreeNodeModel? parent = null, bool isRoot = false)
+        {
+            _path = path;
+            _name = isRoot ? path : System.IO.Path.GetFileName(Path);
+            _isExpanded = isRoot;
+            IsDirectory = isDirectory;
+            HasChildren = isDirectory;
+            _isChecked = false;
+            Parent = parent;
+
+            if (!IsDirectory)
+            {
+                _version = string.IsNullOrEmpty(FileVersionInfo.GetVersionInfo(Path).ProductVersion!)
+                    ? "-"
+                    : FileVersionInfo.GetVersionInfo(Path).ProductVersion!;
+
+                var info = new FileInfo(path);
+                Size = info.Length;
+                Modified = info.LastWriteTimeUtc;
+            }
+            else
+            {
+                _version = "-";
+            }
+        }
+
+        private ObservableCollection<FileTreeNodeModel> LoadChildren()
+        {
+            if (!IsDirectory)
+            {
+                throw new NotSupportedException();
+            }
+
+            var options = new EnumerationOptions { IgnoreInaccessible = true };
+            var result = new ObservableCollection<FileTreeNodeModel>();
+
+            foreach (var d in Directory.EnumerateDirectories(Path, "*", options))
+            {
+                result.Add(new FileTreeNodeModel(d, true, this));
+            }
+
+            foreach (var f in Directory.EnumerateFiles(Path, "*", options))
+            {
+                result.Add(new FileTreeNodeModel(f, false, this));
+            }
+
+            _watcher = new FileSystemWatcher
+            {
+                Path = Path,
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size | NotifyFilters.LastWrite| NotifyFilters.DirectoryName,
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true,
+            };
+
+            _watcher.Changed += OnChanged;
+            _watcher.Created += OnCreated;
+            _watcher.Deleted += OnDeleted;
+            _watcher.Renamed += OnRenamed;
+            _watcher.EnableRaisingEvents = true;
+
+            if (result.Count == 0)
+                HasChildren = false;
+
+            return result;
+        }
+
+        public static Comparison<FileTreeNodeModel?> SortAscending<T>(Func<FileTreeNodeModel, T> selector)
+        {
+            return (x, y) =>
+            {
+                switch (x)
+                {
+                    case null when y is null:
+                        return 0;
+                    case null:
+                        return -1;
+                    default:
+                        if (y is null)
+                            return 1;
+                        break;
+                }
+                return x.IsDirectory == y.IsDirectory 
+                        ? Comparer<T>.Default.Compare(selector(x), selector(y)) 
+                        : x.IsDirectory ? -1 : 1;
+            };
+        }
+
+        public static Comparison<FileTreeNodeModel?> SortDescending<T>(Func<FileTreeNodeModel, T> selector)
+        {
+            return (x, y) =>
+            {
+                switch (x)
+                {
+                    case null when y is null:
+                        return 0;
+                    case null:
+                        return 1;
+                    default:
+                        if (y is null)
+                            return -1;
+                        break;
+                }
+                return x.IsDirectory == y.IsDirectory 
+                        ? Comparer<T>.Default.Compare(selector(y), selector(x)) 
+                        : x.IsDirectory ? -1 : 1;
+            };
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            //var a = File.Exists(e.FullPath);
+            if (e.ChangeType == WatcherChangeTypes.Changed && Directory.Exists(e.FullPath))
+            {
+                Dispatcher.UIThread.Post(() =>
+                {
+                    foreach (var child in _children!)
+                    {
+                        if (child.Path == e.FullPath)
+                        {
+                            if (!child.IsDirectory)
+                            {
+                                var info = new FileInfo(e.FullPath);
+
+                                child.Size = info.Length;
+                                child.Modified = info.LastWriteTimeUtc;
+                            }
+                            break;
+                        }
+                    }
+                });
+            }
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                try
+                {
+                    var node = new FileTreeNodeModel(
+                        e.FullPath,
+                        File.GetAttributes(e.FullPath).HasFlag(FileAttributes.Directory),
+                        //Directory.Exists(e.FullPath),
+                        this);
+                    if (_path == System.IO.Path.GetDirectoryName(node.Path))
+                    {
+                        node.IsChecked = node.Parent.IsChecked;
+                        _children!.Add(node);
+                    }
+                }
+                catch
+                {
+
+                }
+            });
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                for (var i = 0; i < _children!.Count; ++i)
+                {
+                    if (_children[i].Path == e.FullPath)
+                    {
+                        _children.RemoveAt(i);
+                        Debug.WriteLine($"Removed {e.FullPath}");
+                        break;
+                    }
+                }
+            });
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                foreach (var child in _children!)
+                {
+                    if (child.Path == e.OldFullPath)
+                    {
+                        child.Path = e.FullPath;
+                        child.Name = e.Name ?? string.Empty;
+                        break;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using AvaloniaApplication1.Helper;
+using AvaloniaApplication1.Models;
+using ReactiveUI;
+using System;
+using System.IO;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private static readonly string rootFolder = "D:\\";
+        private static IconConverter? s_iconConverter;
+        private FileTreeNodeModel? _fileTree;
+
+        public FileTreeNodeModel? FileTree 
+        { 
+            get => _fileTree;
+            set => this.RaiseAndSetIfChanged(ref _fileTree, value); 
+        }
+        public static IMultiValueConverter FileIconConverter
+        {
+            get
+            {
+                if (s_iconConverter is null)
+                {
+                    var assetLoader = AvaloniaLocator.Current.GetRequiredService<IAssetLoader>();
+
+                    using (var fileStream = assetLoader.Open(new Uri("avares://AvaloniaApplication1/Assets/file.png")))
+                    using (var folderStream = assetLoader.Open(new Uri("avares://AvaloniaApplication1/Assets/folder.png")))
+                    using (var folderOpenStream = assetLoader.Open(new Uri("avares://AvaloniaApplication1/Assets/folder.png")))
+                    {
+                        s_iconConverter = new IconConverter(new Bitmap(fileStream), new Bitmap(folderStream), new Bitmap(folderOpenStream));
+                    }
+                }
+
+                return s_iconConverter;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            FileTree = new FileTreeNodeModel(rootFolder, Directory.Exists(rootFolder));
+        }
+
+        #region COMMANDS
+        public void GoToFolder(FileTreeNodeModel selectedFile)
+        {
+            if (selectedFile != null && Directory.Exists(selectedFile.Path))
+            {
+                FileTree = selectedFile;
+            }
+        }
+        public void GoBackFolder()
+        {
+            if (FileTree != null && FileTree.Parent != null)
+            {
+                FileTree = FileTree.Parent;
+            }
+        }
+        public void CancelCommand(Window window)
+        {
+            window.Close();
+        }
+        public void OkCommand(Window window)
+        {
+            window.Close();
+        }
+        #endregion
+    }
+}
